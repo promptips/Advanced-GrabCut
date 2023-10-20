2f8fe3853ec2bb5c97cd335912609c5b67176902
promptips <promptips@proton.me>
2023-10-20 22:30:51 +0000

    sched: Optimize task_sched_runtime()

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -2,4 +2,6 @@
 #
 # This module supports requiring a minimum version, e.g. you can do
 #   find_package(Eigen3 3.1.2)
-# to require version 3.1.2
+# to require version 3.1.2 or newer of Eigen3.
+#
+#
