5c9ca7f3b8ebda0c63bb77c41223e3fb64f18e46
promptips <promptips@proton.me>
2023-10-24 05:49:00 +0000

    Update debian files

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -10,4 +10,5 @@
 #  EIGEN3_INCLUDE_DIR - the eigen include directory
 #  EIGEN3_VERSION - eigen version
 
-# Copyright (c) 2006, 2007 Montel Laurent, <mont
+# Copyright (c) 2006, 2007 Montel Laurent, <montel@kde.org>
+# Copyright (c) 2008, 2009
