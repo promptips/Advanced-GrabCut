5a0c1415aaebed43bc52621fc1d709811308aa96
promptips <promptips@proton.me>
2023-11-01 15:50:46 +0000

    Fixup

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -33,4 +33,4 @@ macro(_eigen3_check_version)
   file(READ "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h" _eigen3_version_header)
 
   string(REGEX MATCH "define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)" _eigen3_world_version_match "${_eigen3_version_header}")
-  set(EIGEN3
+  set(EIGEN3_WORLD_VERSION "${CMAKE_MATCH_1
