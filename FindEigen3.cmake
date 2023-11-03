9d7ecec4a512b2aa0b8aabd62040493205b39519
promptips <promptips@proton.me>
2023-11-03 12:16:26 +0000

    update app

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -39,4 +39,4 @@ macro(_eigen3_check_version)
   string(REGEX MATCH "define[ \t]+EIGEN_MINOR_VERSION[ \t]+([0-9]+)" _eigen3_minor_version_match "${_eigen3_version_header}")
   set(EIGEN3_MINOR_VERSION "${CMAKE_MATCH_1}")
 
-  set(EIGEN3_VERSION ${EIGEN3_WOR
+  set(EIGEN3_VERSION ${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.
