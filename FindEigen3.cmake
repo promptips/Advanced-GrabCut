0dad77266aaf5ad19b124ac30c27d00180492622
promptips <promptips@proton.me>
2023-11-06 10:30:46 +0000

    Updated gradle

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -51,4 +51,8 @@ macro(_eigen3_check_version)
     message(STATUS "Eigen3 version ${EIGEN3_VERSION} found in ${EIGEN3_INCLUDE_DIR}, "
                    "but at least version ${Eigen3_FIND_VERSION} is required")
   endif(NOT EIGEN3_VERSION_OK)
-endmacro(_eigen3_check_versi
+endmacro(_eigen3_check_version)
+
+if (EIGEN3_INCLUDE_DIR)
+
+  # 
