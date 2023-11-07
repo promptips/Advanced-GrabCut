fc9cbb36381a745b7ec0b935f21a9f9868052949
promptips <promptips@proton.me>
2023-11-07 09:31:43 +0000

    fixed missing comma

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -61,4 +61,6 @@ if (EIGEN3_INCLUDE_DIR)
 
 else (EIGEN3_INCLUDE_DIR)
 
-  find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_libr
+  find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
+      PATHS
+      ${CMAK
