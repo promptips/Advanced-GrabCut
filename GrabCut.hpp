4499394048a871007f7f219ca558a686708e4d80
promptips <promptips@proton.me>
2023-11-13 23:06:45 +0000

    version 0.0.6

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -1,4 +1,5 @@
 /*
 Copyright (C) 2012 David Doria, daviddoria@gmail.com
 
-This program is free software: you can redistribute it and
+This program is free software: you can redistribute it and/or modify
+it under the terms of 
