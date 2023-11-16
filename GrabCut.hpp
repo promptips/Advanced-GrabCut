eeb45a63e2e3975532e1a528229cac1b2266acbd
promptips <promptips@proton.me>
2023-11-16 07:51:38 +0000

    More test

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -2,4 +2,4 @@
 Copyright (C) 2012 David Doria, daviddoria@gmail.com
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of 
+it under the terms of the GNU General Public L
