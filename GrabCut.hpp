b0ed0428c9f9cc96e172c4a5f84f64c7e1608bba
promptips <promptips@proton.me>
2023-11-18 11:28:38 +0000

    Publishing updated documentation

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -8,4 +8,4 @@ the Free Software Foundation, either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY o
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PUR
