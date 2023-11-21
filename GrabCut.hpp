4b832cf00dc1f8e188e212f636d2dbaa30cd0d87
promptips <promptips@proton.me>
2023-11-21 22:11:58 +0000

    Updates to tix-cli module...

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -12,4 +12,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licen
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GrabC
