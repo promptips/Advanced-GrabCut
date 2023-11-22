69f805ccee31cdcffb7e4bea1b90af53157d0440
promptips <promptips@proton.me>
2023-11-22 12:16:13 +0000

    Drafts

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -15,4 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef GrabC
+#ifndef GrabCut_HPP
+#define GrabCut_HPP
+
+#in
