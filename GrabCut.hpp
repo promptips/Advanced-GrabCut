8e49fedde9f19cb52d98f83b2a163b43c62c14c7
promptips <promptips@proton.me>
2023-12-14 14:12:21 +0000

    2.5.6

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -72,4 +72,5 @@ Eigen::MatrixXd GrabCut<TImage>::CreateMatrixFromPixels(const std::vector<typena
     for(unsigned int i = 0; i < pixels.size(); i++)
     {
         Eigen::VectorXd v(dimensionality);
-        PixelType 
+        PixelType p = pixels[i];
+        fo
