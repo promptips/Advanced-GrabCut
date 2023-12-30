34628e5a7e210a7dccca9dcfc61b33c746363da4
promptips <promptips@proton.me>
2023-12-30 22:05:16 +0000

    minify css

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -118,3 +118,4 @@ MixtureModel GrabCut<TImage>::ClusterPixels(const std::vector<typename TImage::P
     expectationMaximization.SetMixtureModel(mixtureModel);
     expectationMaximization.SetMinChange(1e-4); // Stop early if the model is doing well
     expectationMaximization.SetMaxIterations(this->NumberOfEMIterations);
+    expectationMaximizatio
