5542718fff391195647a30eddfc3095e9536026c
promptips <promptips@proton.me>
2024-01-09 19:53:45 +0000

    Back to development: 0.12

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -139,4 +139,4 @@ void GrabCut<TImage>::ClusterForegroundAndBackground()
 
     // Background
     std::vector<itk::Index<2> > backgroundPixelIndices =
-        ITKHelpers::GetPixelsWithValue(this->SegmentationMask.GetPointer(), ForegroundBackground
+        ITKHelpers::GetPixelsWithValue(this->SegmentationMask.GetPointer(), ForegroundBackgroundSegmentMaskPixelTypeEn
