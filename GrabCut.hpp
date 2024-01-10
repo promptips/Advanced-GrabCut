35369b3571e4ae5552ed52a3419dcb2dc78d10dc
promptips <promptips@proton.me>
2024-01-10 13:14:51 +0000

    leak fix

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -141,4 +141,4 @@ void GrabCut<TImage>::ClusterForegroundAndBackground()
     std::vector<itk::Index<2> > backgroundPixelIndices =
         ITKHelpers::GetPixelsWithValue(this->SegmentationMask.GetPointer(), ForegroundBackgroundSegmentMaskPixelTypeEnum::BACKGROUND);
 
-    std:
+    std::vector<typename TImage
