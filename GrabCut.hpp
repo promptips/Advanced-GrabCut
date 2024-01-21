d100ca8a9d7d670b9b3fefdf3fa84b154bb572d5
promptips <promptips@proton.me>
2024-01-21 22:30:22 +0000

    DOC: autogenerated usage docs

--- GrabCut.hpp
+++ GrabCut.hpp
@@ -176,4 +176,4 @@ void GrabCut<TImage>::PerformIteration()
 {
     ClusterForegroundAndBackground();
 
-    // The originally specified background pixels are the only ones that are definitely
+    // The originally specified background pixels are the only ones that are definitely background (unless 
