4ea47dd5e7efc0dc46b6d7d1989572e0eda153d1
promptips <promptips@proton.me>
2024-02-13 11:40:46 +0000

    Update lesson1.md

--- GrabCutExample.cpp
+++ GrabCutExample.cpp
@@ -36,3 +36,39 @@ int main(int argc, char*argv[])
     std::cerr << "Required: image.png mask.fgmask output.png" << std::endl;
     return EXIT_FAILURE;
   }
+
+  // Parse arguments
+  std::string imageFilename = argv[1];
+
+  // This image should have white pixels indicating foreground pixels and be black elsewhere.
+  std::string maskFilename = argv[2];
+
+  std::string outputFilename = argv[3];
+
+  // Output arguments
+  std::cout << "imageFilename: " << imageFilename << std::endl
+            << "maskFilename: " << maskFilename << std::endl
+            << "outputFilename: " << outputFilename << std::endl;
+
+  // The type of the image to segment
+  typedef itk::Image<itk::CovariantVector<unsigned char, 3>, 2> ImageType;
+
+  // Read the image
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName(imageFilename);
+  reader->Update();
+
+  // Read the mask
+  ForegroundBackgroundSegmentMask::Pointer mask =
+      ForegroundBackgroundSegmentMask::New();
+  mask->Read(maskFilename);
+
+  // Perform the segmentation
+  std::cout << "Starting GrabCut..." << std::endl;
+  GrabCut<ImageType> grabCut;
+  grabCut.SetImage(reader->GetOutput());
+  grabCut.SetInitialMask(mask);
+  grabCut.PerformSegmentation();
+
+  // Get and write the result
