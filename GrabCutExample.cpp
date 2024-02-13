ca860062498d95ac9c8d3f069377cea187da4796
promptips <promptips@proton.me>
2024-02-13 10:58:55 +0000

    sparc32: vm_area_struct access for old Sun SPARCs.

--- GrabCutExample.cpp
+++ GrabCutExample.cpp
@@ -11,3 +11,28 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "GrabCut.h"
+
+// Submodules
+#include "Mask/ITKHelpers/ITKHelpers.h"
+
+// ITK
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+#include "itkVectorImage.h"
+
+int main(int argc, char*argv[])
+{
+  // Verify arguments
+  if(argc != 4)
+  {
+    std::cerr << "Required: image.png mask.fgmask output.png" << std::endl;
+    return EXIT_FAILURE;
+  }
