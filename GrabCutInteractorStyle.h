b2fac5f2a535eabaf3c27e3c1f3c4e94977f6031
promptips <promptips@proton.me>
2024-02-29 09:19:28 +0000

    Main script

--- GrabCutInteractorStyle.h
+++ GrabCutInteractorStyle.h
@@ -45,4 +45,4 @@ protected:
   vtkSmartPointer<vtkRenderer> RightRenderer;
   vtkSmartPointer<vtkImageActor> ImageActor;
   vtkSmartPointer<vtkImageActor> ClippedActor;
-  vtk
+  vtkSmartPointer<vtkImageCl
