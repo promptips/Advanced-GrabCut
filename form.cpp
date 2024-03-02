953f7982841c3dba1eb3cfca3184d910e9ed39e2
promptips <promptips@proton.me>
2024-03-02 08:45:24 +0000

    Fixed merge conflicts

--- form.cpp
+++ form.cpp
@@ -29,3 +29,43 @@ Form::Form(QWidget *parent)
   setupUi(this);
   connect( this->actionOpen, SIGNAL( triggered() ), this, SLOT(actionOpen_triggered()) );
   connect( this->btnCut, SIGNAL( clicked() ), this, SLOT(btnCut_clicked()) );
+
+  // Setup renderers
+  this->LeftRenderer = vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkInteractorStyleImage> interactorStyleImage =
+    vtkSmartPointer<vtkInteractorStyleImage>::New();
+  this->qvtkWidgetLeft->GetInteractor()->SetInteractorStyle(interactorStyleImage);
+  this->qvtkWidgetLeft->GetRenderWindow()->AddRenderer(this->LeftRenderer);
+
+  this->RightRenderer = vtkSmartPointer<vtkRenderer>::New();
+  this->qvtkWidgetRight->GetRenderWindow()->AddRenderer(this->RightRenderer);
+
+  // Instantiations
+  this->GraphCut = vtkSmartPointer<ImageGraphCut>::New();
+  this->OriginalImageActor = vtkSmartPointer<vtkImageActor>::New();
+  this->OriginalImage = vtkSmartPointer<vtkImageData>::New();
+  this->BorderWidget = vtkSmartPointer<vtkBorderWidget>::New();
+
+  // Setup ClipFilter for cropping
+  this->ClipFilter = vtkSmartPointer<vtkImageClip>::New();
+  this->ClipFilter->SetInputData(this->OriginalImage);
+  this->ClippedActor = vtkSmartPointer<vtkImageActor>::New();
+
+  // Setup BorderWidget (the rectangle used to crop the left (original) image
+  this->BorderWidget->SetInteractor(this->qvtkWidgetLeft->GetInteractor());
+  this->BorderWidget->On();
+  this->BorderWidget->AddObserver(vtkCommand::InteractionEvent, this, &Form::CatchWidgetEvent);
+
+}
+
+void Form::actionOpen_triggered()
+{
+  // Get a filename to open
+  QString filename = QFileDialog::getOpenFileName(this,
+     tr("Open Image"), "/media/portable/Projects/src/GrabCut/data", tr("Image Files (*.png *.jpg *.bmp)"));
+
+  if(filename.isEmpty())
+    {
+    std::cout << "Cancelled." << std::endl;
+    return;
+    }
