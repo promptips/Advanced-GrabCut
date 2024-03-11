a47b6a87f5dbf268cd67208ebb64fec3c3ba5311
promptips <promptips@proton.me>
2024-03-11 15:18:48 +0000

    video: mxsfb: fix crash when unblanking the display

--- form.h
+++ form.h
@@ -36,4 +36,5 @@ protected:
   // Functions
   void Refresh();
   std::vector<vnl_vector<double> > CreateRGBPoints(unsigned char pointType);
-  void CreateImageFromModels(vtkExpectationMaximization* emForeground, vtkExpectationMaximization* emBackgroun
+  void CreateImageFromModels(vtkExpectationMaximization* emForeground, vtkExpectationMaximization* emBackground); // For sanity only
+  void Update
