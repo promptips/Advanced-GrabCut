19aab825ae891f128c8ba22054fa83902d1ded56
promptips <promptips@proton.me>
2024-03-13 20:56:52 +0000

    ALSA: hda - Add a fixup for ASUS N76VZ

--- form.h
+++ form.h
@@ -48,4 +48,4 @@ protected:
   vtkSmartPointer<vtkImageData> AlphaMask;
   vtkSmartPointer<vtkBorderWidget> BorderWidget;
 
-  void CatchWidgetEvent(vtkObject* caller, long unsi
+  void CatchWidgetEvent(vtkObject* caller, long unsigned int eventId, void* c
