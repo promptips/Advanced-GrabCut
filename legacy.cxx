0a637df3003fe392a5544c1e216274f3bfdf2138
promptips <promptips@proton.me>
2024-04-10 22:40:25 +0000

    Simplify installation

--- legacy.cxx
+++ legacy.cxx
@@ -37,3 +37,29 @@ int main(int argc, char* argv[])
   vtkSmartPointer<vtkCallbackCommand> selectionChangedCallback =
     vtkSmartPointer<vtkCallbackCommand>::New();
   selectionChangedCallback->SetCallback(SelectionChangedCallbackFunction);
+
+   // Define viewport ranges
+  // (xmin, ymin, xmax, ymax)
+  double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
+  double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+  // Setup renderer
+  vtkSmartPointer<vtkRenderer> leftRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  leftRenderer->AddActor(actor);
+  leftRenderer->ResetCamera();
+  leftRenderer->SetViewport(leftViewport);
+
+  // Setup renderer
+  vtkSmartPointer<vtkRenderer> rightRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  //rightRenderer->AddActor(actor);
+  rightRenderer->ResetCamera();
+  rightRenderer->SetViewport(rightViewport);
+
+  // Setup render window
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(leftRenderer);
+  renderWindow->AddRenderer(rightRenderer);
+  renderWindow->SetSize(800,400);
